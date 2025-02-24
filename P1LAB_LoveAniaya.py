6233d6ae52a491e295b18a3530b736f341075a5e
Anaiyalove123 <lovea1934@student.faytechcc.edu>
2025-02-23 22:29:55 -0500

    Add files via upload

---
+++ P1LAB_LoveAniaya.py
@@ -0,0 +1,8 @@
+#Anaiya Love
+#Febuary 23, 2025
+#P1LAB1
+#Learning how to create a Python program using IDLE.
+first_name = ("Anaiya")
+last_name = ("Love")
+print(f"Hello, Welcome to CTI-110, {first_name} {last_name}!")
+Hello, Welcome to CTI-110, Anaiya Love!
